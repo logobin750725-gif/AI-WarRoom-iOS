992e6d0a02aef4eb8b1cd95c99c58e88a1996cc9
logobin750725-gif <logobin750725@gmail.com>
2026-09-25 07:57:26 +0800

    Add Scheme 2 app entry

---
+++ Scheme2/App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct Scheme2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(title: "Scheme 2", subtitle: "4 Companies War Room + Judge", resource: "scheme2")
+        }
+    }
+}
