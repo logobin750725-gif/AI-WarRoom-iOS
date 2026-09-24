4421bc6c308174c603cc547f58191a8ddff6f1d9
logobin750725-gif <logobin750725@gmail.com>
2026-09-25 07:57:24 +0800

    Add Scheme 1 app entry

---
+++ Scheme1/App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct Scheme1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(title: "Scheme 1", subtitle: "Same Model × 4 Persona", resource: "scheme1")
+        }
+    }
+}
